794978515c6bf8dbd655f93743db5d5e9a9d0d1f
Shuya-Ma <30282686+Shuya-Ma@users.noreply.github.com>
2017-08-01 21:06:15 -0500

    test

--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -16,8 +16,8 @@
 				<div class= "big_title">
 					<h2 class="big_title">TECHMOLOGY SUMMARY</h2>
 				</div>
-				<div class="section_content">
-				
+				<div class="section_content_tech_sum">
+
 				</div>
 			</div>
 
@@ -25,17 +25,38 @@
 				<div class= "big_title">
 					<h2 class="big_title">EDUCATION</h2>
 				</div>
+
+				<div class="section_content_edu">
+					<div class="edu_left">
+						<h3 class="school_name">Washington University in St. Louis</h3>
+						<p class="degree">Bachelor of Arts: Mathematics</p>
+						<p> Second Major in Computer Science and Minor in Psychology</p>
+					</div>
+
+					<div class="edu_right">
+						<p>
+
+					</div>
+
+
+				</div>
 			</div>
 
 			<div class="big_sec">
 				<div class= "big_title">
 					<h2 class="big_title">PROFESSIONAL EXPERIENCE</h2>
 				</div>
+				<div class="section_content_work">
+
+				</div>
 			</div>
 
 			<div class="big_sec">
 				<div class= "big_title">
 					<h2 class="big_title">VOLUNTEER EXPERIENCE</h2>
+				</div>
+				<div class="section_content_volunteer">
+
 				</div>
 			</div>
 
